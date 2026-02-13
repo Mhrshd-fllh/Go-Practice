38d7b72baa0848b888b82e39d0e6e6758f946cf8
Mehrshad Fallah Estalkhzir <fallah_mehrshad82@comp.iust.ac.ir>
2026-02-13 17:31:41 +0330

    feat: adding post for users

--- 6/cmd/api/main.go
+++ 6/cmd/api/main.go
@@ -7,15 +7,18 @@ import (
 	"project/internal/store"
 )
 
+const version = "0.0.1"
+
 func main() {
 	cfg := config{
-		addr: env.GetString("ADDR", ":8080"),
+		addr: env.GetString("ADDR", ":3000"),
 		db: dbConfig{
 			addr:         env.GetString("DB_ADDR", "postgres://admin:adminpassword@localhost:/socialnetwork?sslmode=disable"),
 			maxOpenConns: env.GetInt("DB_MAX_OPEN_CONNS", 30),
 			maxIdleConns: env.GetInt("DB_MAX_IDLE_CONNS", 15),
 			maxIdleTime:  env.GetString("DB_MAX_IDLE_TIME", "15m"),
 		},
+		env: env.GetString("ENV", "development"),
 	}
 
 	db, err := db.New(cfg.db.addr, cfg.db.maxOpenConns, cfg.db.maxIdleConns, cfg.db.maxIdleTime)
